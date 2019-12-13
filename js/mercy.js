c06258e95efff164c70629da77bc6e348f041cbd
unknown <wilsonosasume95@gmail.com>
2019-12-13 09:39:05 -0800

    mercy project

---
+++ js/mercy.js
@@ -0,0 +1,8 @@
+let navbar = document.querySelector('ul');
+let navbarClass = navbar.classList;
+let navbarChange = document.querySelector('.navbar-change')
+let navbarToggle = document.querySelector('#navbar-toggle')
+
+navbarToggle.addEventListener('click', () =>{
+    navbar.classList.toggle('navbarChange');
+});
